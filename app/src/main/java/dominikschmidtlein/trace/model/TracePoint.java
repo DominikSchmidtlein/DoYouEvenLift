998dd8b210db157ee1db09259d990ffeeaab9837
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2016-12-18 20:42:31 -0500

    changed privacy to package

--- app/src/main/java/dominikschmidtlein/trace/model/TracePoint.java
+++ app/src/main/java/dominikschmidtlein/trace/model/TracePoint.java
@@ -8,38 +8,29 @@ import java.util.Set;
 /**
  * Created by domin_2o9sb4z on 2016-11-23.
  */
-public class TracePoint {
+class TracePoint extends Point{
 
     private double x;
     private double y;
 
     private Set<Connection> connections;
 
-    public static final int DEFAULT_CAPACITY = 16;
+    static final int DEFAULT_CAPACITY = 16;
 
-    public TracePoint(double x, double y) {
+    TracePoint(double x, double y) {
         this(x, y, DEFAULT_CAPACITY);
     }
 
-    public TracePoint(double x, double y, int capacity) {
-        this.x = x;
-        this.y = y;
+    TracePoint(double x, double y, int capacity) {
+        super(x, y);
         this.connections = new HashSet<>(capacity);
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void addConnection(Connection connection) {
+    void addConnection(Connection connection) {
         connections.add(connection);
     }
 
-    public Connection connectedTo(TracePoint tracePoint) {
+    Connection connectedTo(TracePoint tracePoint) {
         for (Connection connection : connections) {
             if (connection.connects(this, tracePoint)) {
                 return connection;
@@ -48,21 +39,7 @@ public class TracePoint {
         return null;
     }
 
-    public double distanceTo(@NonNull TracePoint tracePoint) {
-        return Math.hypot(getX() - tracePoint.getX(), getY() - tracePoint.getY());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof TracePoint)) {
-            return false;
-        }
-        TracePoint tp = (TracePoint) o;
-        return getX() == tp.getX() && getY() == tp.getY();
-    }
-
-    @Override
-    public int hashCode() {
-        return (int)(x * 997 + y * 83);
+    public Set<Connection> getConnections() {
+        return connections;
     }
 }
