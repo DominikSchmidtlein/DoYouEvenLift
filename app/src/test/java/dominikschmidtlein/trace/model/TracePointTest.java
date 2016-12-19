a20aadcd71e3101aef582c444c87fb79785d8e9f
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2016-12-18 20:44:23 -0500

    abstracted tests from TracePoint to Point

--- app/src/test/java/dominikschmidtlein/trace/model/TracePointTest.java
+++ app/src/test/java/dominikschmidtlein/trace/model/TracePointTest.java
@@ -23,33 +23,10 @@ public class TracePointTest {
 
     @Test
     public void testConnectedTo() {
-        assertNull(point1.connectedTo(null));
         assertNull(point1.connectedTo(point1));
         assertNotNull(point1.connectedTo(point2));
         assertNotNull(point2.connectedTo(point1));
         assertNotNull(point2.connectedTo(new TracePoint(100, 100)));
-    }
-
-    @Test
-    public void testDistanceTo() {
-        assertEquals(point1.distanceTo(point1), 0, 0.0000001);
-        assertEquals(point1.distanceTo(point2), 100, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(100, 150)), 50, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(100, 50)), 50, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(50, 100)), 50, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(150, 100)), 50, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(50, 50)), 70.7106781187, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(50, 150)), 70.7106781187, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(150, 50)), 70.7106781187, 0.0000001);
-        assertEquals(point1.distanceTo(new TracePoint(150, 150)), 70.7106781187, 0.0000001);
-    }
-
-        @Test
-    public void testEquals() {
-        assertNotEquals(point1, null);
-        assertNotEquals(point1, point2);
-        assertEquals(point1, new TracePoint(100, 100));
-        assertEquals(point1, point1);
     }
 
 }
